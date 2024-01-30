b1dd16d6d6dc34cfdf28667831e5bc2595e67a9d
le.quang.dao <le.quang.dao@sun-asterisk.com>
2024-01-30 09:45:38 +0700

    [Add] Upgrade to latest version for library

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,15 +4,15 @@ object Versions {
     const val gradlePlugin = "8.2.1"
     const val kotlin = "1.9.22"
 
-    const val coreKtx = "1.9.0"
+    const val coreKtx = "1.12.0"
     const val appCompat = "1.6.1"
-    const val material = "1.8.0"
+    const val material = "1.11.0"
     const val constraintLayout = "2.1.4"
-    const val coreSplashScreen = "1.0.0"
+    const val coreSplashScreen = "1.12.0"
 
-    const val lifecycle = "2.5"
-    const val navigation = "2.5.3"
-    const val koin = "3.4.0"
+    const val lifecycle = "2.7.0"
+    const val navigation = "2.7.6"
+    const val koin = "3.5.3"
 
     const val jUnit = "4.13.2"
     const val mockk = "1.13.2"
