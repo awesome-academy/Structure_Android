a5fe6846c25d1726d880cc5a933f77dafc58e8a8
le.quang.dao <le.quang.dao@sun-asterisk.com>
2024-03-28 14:13:51 +0700

    [fix] remove unuse code

--- app/src/main/java/com/sun/android/ui/listmovie/MoviesViewModel.kt
+++ app/src/main/java/com/sun/android/ui/listmovie/MoviesViewModel.kt
@@ -1,6 +1,5 @@
 package com.sun.android.ui.listmovie
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sun.android.data.MovieRepository
@@ -8,8 +7,6 @@ import com.sun.android.data.model.Movie
 import com.sun.android.utils.LogUtils
 import com.sun.android.utils.dispatchers.DispatcherProvider
 import com.sun.android.utils.livedata.SingleLiveData
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
@@ -18,16 +15,9 @@ class MoviesViewModel(private val movieRepository: MovieRepository) : ViewModel(
     val movies = SingleLiveData<List<Movie>>()
 
     fun requestTopRateMovies() {
-        val exceptionHandler = CoroutineExceptionHandler { context, error ->
-            // Do what you want with the error
-            Log.e("TAG", error.toString())
-        }
-
-        val supervisor = SupervisorJob()
-
-        viewModelScope.launch(exceptionHandler + supervisor) {
+        viewModelScope.launch {
             movieRepository.getMovies().catch { e ->
-                LogUtils.e("QQQQQ", e.toString())
+                LogUtils.e("requestTopRateMovies", e.toString())
             }.flowOn(DispatcherProvider().io())
                 .collect {
                     movies.value = it
--- app/src/main/java/com/sun/android/utils/Constant.kt
+++ app/src/main/java/com/sun/android/utils/Constant.kt
@@ -2,6 +2,6 @@ package com.sun.android.utils
 
 object Constant {
     const val DATABASE_NAME = "movies_database"
-    const val BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500"
+    const val BASE_URL_IMAGE = "image.tmdb.org/t/p/w500"
     const val BASE_API_KEY = "608dc3a0e4c39fe0b691a89554ec2b1f"
 }
